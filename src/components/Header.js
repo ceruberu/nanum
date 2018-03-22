e038af01332ace7471db69ccec9cf5e75f22ecdf
Duck Yeon Kim <kduckyeon@gmail.com>
2018-03-23 05:12:48 +0900

    add responsive search bar logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Address from '../components/Address';
+import 'font-awesome/css/font-awesome.min.css';
 import './Header.css';
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isSearching: false
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(!prevState.isSearching && this.state.isSearching){
+      this.textInput.focus();
+    }
+  }
+
+  clickSearch(){
+    this.setState({
+      isSearching: true
+    })
+  }
+
+  clickBack(){
+    this.setState({
+      isSearching: false
+    })
+  }
+
   render() {
-    return (
-        <div className="header">
-          <div className="headerTitle">나눔</div>
-          <NavLink to="/" className="headerLink"> 받기 </NavLink>
-          <NavLink to="/share" className="headerLink"> 나누기 </NavLink>
-        </div>
-    );
+
+    return [
+      <div className="header" key="1234">
+        <div className="headerTitle">나눔</div>
+        <NavLink to="/" className="headerLink"> 받기 </NavLink>
+        <NavLink to="/share" className="headerLink shareLink"> 나누기 </NavLink>
+        <form className="search-container" show-input={`${this.state.isSearching}`}>
+          <button type="button" className="back-button" onClick={()=>this.clickBack()}>
+            <i className="fas fa-arrow-left"></i>
+          </button>
+          <input 
+            className="search-bar" 
+            type="text" 
+            ref={text => this.textInput = text}
+            placeholder="Search" />
+          <button className="search-button" onClick={()=>this.search()}>
+            <i className="nav-search fa fa-search" />
+          </button>
+        </form>
+        <button className="show-search-button" onClick={()=>this.clickSearch()}>
+          <i className="nav-search fa fa-search"/>
+        </button>
+      </div>
+      ,
+      <div className="header-nav" key="123455">
+        <div className="nav-icon"> 최신</div>
+        <div className="nav-icon"> 인기</div>
+        <Address />
+      </div>
+    ]
   }
 }
 
